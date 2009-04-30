73683ab0c5971801e453aad468e2f486033e459e
Tim van Werkhoven <t.i.m.vanwerkhoven@xs4all.nl>
2009-04-30 09:00:29 +0200

    Jot script for testing C library.

--- jot.py
+++ jot.py
@@ -1,5 +1,22 @@
 
-# load SA / SF config
+import pyana
+import libsh
+import Clibshifts
 
-(nsa, saccdpos, saccdsize) = libsh.loadSaSfConf('./2009.04.22-mask.csv')
-(nsf, sfccdpos, sfccdsize) = libsh.loadSaSfConf('2009.04.22-subfield-big.csv')
+pdir = './data/2009.04.27/proc/'
+ddir = './data/2009.04.27/'
+imfile = 'wfwfs_test_im27Apr2009.0001000'
+
+# load SA / SF config
+(nsa, saccdpos, saccdsize) = libsh.loadSaSfConf(pdir+'2009.04.22-mask.csv')
+(nsf, sfccdpos, sfccdsize) = \
+ 	libsh.loadSaSfConf(pdir+'2009.04.22-subfield-big.csv')
+
+# load image 
+img = pyana.load(ddir+imfile)
+
+# Pass to C library
+
+Clibshifts.calcShifts(img, saccdpos, saccdsize, sfccdpos, sfccdsize, N.array([4,4]))
+
+# done
